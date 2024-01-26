2867320144aa3f25e8aa5c83e105130c129c5759
quan442644 <quancab1508@gmail.com>
2024-01-26 14:45:23 +0700

    Nội dung commit của bạn ở đây

---
+++ SQLQuery2.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE HocSinh (
+Ten varchar(100),
+Diachi varchar(255),
+Cmnd varchar(100),
+ngaysinh DATE
+);
+INSERT INTO HocSinh (Ten, Diachi, Cmnd,ngaysinh)
+VALUES ('nva', 'hcm', '1','2003-2-2');
+INSERT INTO HocSinh (Ten, Diachi, Cmnd)
+VALUES ('nva2', 'hcm', '2');
+INSERT INTO HocSinh (Ten, Diachi, Cmnd)
+VALUES ('nva3', 'hcm', '3');
+INSERT INTO HocSinh (Ten, Diachi, Cmnd)
+VALUES ('nva4', 'hcm', '4');
+INSERT INTO HocSinh (Ten, Diachi, Cmnd)
+VALUES ('nva5', 'hcm', '5');
+
+select * from HocSinh
+drop table HocSinh;
+
---
+++ SQLQuery4.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[GiangVien] (
+    [Ten]      VARCHAR (100) NULL,
+    [Diachi]   VARCHAR (255) NULL,
+    [Cmnd]     VARCHAR (100) NULL,
+    [ngaysinh] DATE          NULL
+);
